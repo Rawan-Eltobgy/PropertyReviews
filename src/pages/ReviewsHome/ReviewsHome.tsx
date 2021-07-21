8916be635d44cee063a938305a5ae28bff6b34a7
Rawan Eltobgy <rawan.eltobgy@harmonica.life>
2021-07-21 04:15:29 +0200

    fix: Fix bg maxwidth issues

--- src/pages/ReviewsHome/ReviewsHome.tsx
+++ src/pages/ReviewsHome/ReviewsHome.tsx
@@ -4,23 +4,17 @@ import { ReviewsList } from "../../components";
 function ReviewsHome() {
   return (
     <div className={styles.Container}>
-      {/* Header Background (bg img + id+ title) **/}
       <div className={styles.Container__background}>
         <div className={styles.Container__darkOverlay}>
           <div className={styles.Container__backgroundTextView}>
-            <h3 className={styles.Container__reviewID}>ID: 091021</h3>
-            <h2 className={styles.Container__reviewTitle}>
+            <h4 className={styles.Container__reviewID}>ID: 091021</h4>
+            <h1 className={styles.Container__reviewTitle}>
               La Casa de las Flores
-            </h2>
+            </h1>
           </div>
         </div>
         <ReviewsList />
       </div>
-
-      {/* Card View with reviews number and card for each review **/}
-
-      {/* Add  filter by score, channel **/}
-      {/* Add pagination at the end of the page  **/}
     </div>
   );
 }
